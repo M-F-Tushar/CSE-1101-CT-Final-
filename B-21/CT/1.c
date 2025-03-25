26852c0a7f59524f2031923a6cdb76cb10fad52c
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-25 18:42:26 +0600

    Add files via upload

---
+++ B-21/CT/1.c
@@ -0,0 +1,13 @@
+
+1. Rules of Identifiers in C
+Identifiers are the names used to identify variables, functions, arrays, and other user-defined elements in a C program. The rules for identifiers are:
+
+Must start with a letter (A-Z or a-z) or an underscore (_) but cannot start with a digit (0-9).
+
+Can contain letters, digits, and underscores (_) but no special characters (e.g., @, #, $).
+
+Cannot be a C keyword (e.g., int, float, return).
+
+C is case-sensitive, meaning Variable and variable are different identifiers.
+
+Must not exceed compiler-defined length limits (usually 31 characters for portability).
