2efdd3e01231941128290119f8dd8551d571a112
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-25 18:44:37 +0600

    Add files via upload

---
+++ B-21/CT-1/3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+    int i = -1, j = -1, k = 2, l = 5, m;
+    m = i++ && ++j || k++ && --l;
+    printf("%d %d %d %d %d", i, j, k, l, m);
+    return 0;
+}
+Step-by-Step Execution
+i++: -1 && (post-increments i to 0 but evaluates as true).
+
+++j: j = 0, evaluates as false, so the first part i++ && ++j is false.
+
+Since false ||, we move to k++ && --l:
+
+k++: 2 &&, post-increments k = 3 but evaluates as true.
+
+--l: l = 4, evaluates as true, so true && true = true.
+
+m = 1.
+
+Final Values
+ini
+Copy
+Edit
+i = 0, j = 0, k = 3, l = 4, m = 1
+Output
+Copy
+Edit
+0 0 3 4 1
