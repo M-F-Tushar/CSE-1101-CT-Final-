c4c6d07556daba5beca4edf1c22cfe06aab950c4
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-06 21:08:03 +0600

    Update 1.c

--- B-21/CT-3/1.c
+++ B-21/CT-3/1.c
@@ -1,53 +1,28 @@
 1. C Program to Check Whether a Given String is a Palindrome
-
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-
-// Function to check if a string is a palindrome
-int isPalindrome(char str[]) {
-    int left = 0, right = strlen(str) - 1;
-
-    while (left < right) {
-        // Convert to lowercase to handle case-insensitive comparison
-        if (tolower(str[left]) != tolower(str[right])) {
-            return 0;  // Not a palindrome
-        }
-        left++;
-        right--;
-    }
-    return 1;  // Palindrome
-}
 
 int main() {
     char str[100];
+    int i, length, isPalindrome = 1;
 
     printf("Enter a string: ");
-    gets(str);  // Read input string
+    scanf("%s", str);
 
-    if (isPalindrome(str)) {
-        printf("The string is a palindrome.\n");
-    } else {
-        printf("The string is not a palindrome.\n");
+    length = strlen(str);
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
     }
+
+    if (isPalindrome)
+        printf("'%s' is a palindrome.\n", str);
+    else
+        printf("'%s' is not a palindrome.\n", str);
 
     return 0;
 }
-Explanation
-The isPalindrome() function compares characters from both ends of the string.
 
-It ignores case differences using tolower().
-
-The main() function reads user input and calls isPalindrome().
-
-Output whether the string is a palindrome.
-
-Example Runs
-csharp
-Copy
-Edit
-Enter a string: madam
-The string is a palindrome.
-
-Enter a string: Hello
-The string is not a palindrome.
