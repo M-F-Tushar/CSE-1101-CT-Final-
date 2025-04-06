134df328affced992742884cc2619156af1fa8f2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-06 10:22:03 +0600

    Update 3.c

--- B-21/Final/3.c
+++ B-21/Final/3.c
@@ -1,4 +1,16 @@
-3. Compute Displacement at Double the Given Time
+/*
+3. A particle has initial velocity and acceleration. If its velocity after a certain time is v, then what will its displacement be in twice that time? 
+
+The input will contain two integers in each line. Each line makes one set of input. These two integers denote the value of v (100 <= v <= 100) and t (0 < t <= 200) (t means at the time the particle gains that velocity). 
+
+For each line of input, print a single integer in one line denoting the displacement in double of that time.
+
+Sample Input: 
+0 0 5 12
+
+Sample Output: 
+0 120
+    
 Explanation:
 Using kinematics, the displacement formula is:
 
@@ -9,20 +21,26 @@ t is the given time.
 a is acceleration, calculated as
 a= v/t.
 Displacement at 2t is calculated using the formula for displacement.
-
+*/
 #include <stdio.h>
-
-void calculateDisplacement(int v, int t) {
-    int a = v / t;  // Acceleration
-    int displacement = (v * 2 * t) - (0.5 * a * (2 * t) * (2 * t));
-    printf("%d\n", displacement);
-}
 
 int main() {
     int v, t;
-    while (scanf("%d %d", &v, &t) != EOF) {
-        printf("Displacement: ");
-        calculateDisplacement(v, t);
+
+    // Let's assume we want to process 3 pairs for this example
+    for (int i = 0; i < 3; i++) {
+        scanf("%d %d", &v, &t);
+
+        if (t == 0) {
+            printf("0\n");
+        } else {
+            double a = (double)v / t;
+            double time = 2 * t;
+            double displacement = (v * time) - (0.5 * a * time * time);
+            printf("%d\n", (int)displacement);
+        }
     }
+
     return 0;
 }
+
