7d6c28b6ce0bfe79e505d48e834b79ce5c66b334
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-07 08:52:28 +0600

    Update 1.c

--- B-22/CT-2/1.c
+++ B-22/CT-2/1.c
@@ -1,15 +1,11 @@
 1. Distinguish between while and do-while loop.
 Answer:
-Feature	while Loop	do-while Loop
-Execution Condition	The condition is checked before entering the loop body.	The loop body executes at least once, then the condition is checked.
-Execution Guarantee	May not execute if the condition is false initially.	Always executes at least once.
-Syntax	c while(condition) { // code }	c do { // code } while(condition);
+Feature	                while Loop	                                                        do-while Loop
+Execution Condition	    The condition is checked before entering the loop body.	            The loop body executes at least once, then the condition is checked.
+Execution               Guarantee	May not execute if the condition is false initially.	Always executes at least once.
+Syntax	                while(condition) { // code }	c do { // code } while(condition);
 Example:
 while loop
-
-c
-Copy
-Edit
 int i = 5;
 while (i < 5) {
     printf("%d\n", i);
@@ -18,10 +14,6 @@ while (i < 5) {
 Output: (Nothing, because i is not less than 5)
 
 do-while loop
-
-c
-Copy
-Edit
 int i = 5;
 do {
     printf("%d\n", i);
