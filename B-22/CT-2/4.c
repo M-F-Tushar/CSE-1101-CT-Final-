318c9aaf3cb8dab7b80a171ba1c85602b0e88552
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-29 08:04:47 +0600

    Update 4.c

--- B-22/CT-2/4.c
+++ B-22/CT-2/4.c
@@ -12,14 +12,18 @@ int main() {
     
     // Take input for the array elements
     printf("Enter the elements of the array:\n");
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
 
     // Sort the array using Bubble Sort
-    for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
+    for(i = 0; i < n - 1; i++) 
+    {
+        for(j = 0; j < n - i - 1; j++) 
+        {
+            if(arr[j] > arr[j + 1]) 
+            {
                 // Swap elements if they are in the wrong order
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -30,7 +34,8 @@ int main() {
 
     // After sorting, the last element is the maximum value
     printf("Sorted array: ");
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
